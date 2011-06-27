7be70b2f088e199a9a6a79bf686dd87cddd69581
Mr Rogers <jon@bunnymatic.com>
2011-06-26 18:19:20 -0700

    * revamped where the config is set  * added rake file so we can run this app from the cmdline with rake run

--- config.ru
+++ config.ru
@@ -1,12 +1,9 @@
 require 'rubygems'
 require 'sinatra'
+require 'datamapper'
 
-set :environment, :production
-set :port, 5000
 disable :run, :reload
 
 require './the_app.rb'
-
-DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/the_app.db")
 
 run TheApp.new
--- the_app.rb
+++ the_app.rb
@@ -1,7 +1,17 @@
 require 'sinatra'
+require 'datamapper'
 
 class TheApp < Sinatra::Base
+  set :environment, :production
+  set :logging, true
+  set :root, Dir.pwd
+  set :port, 5678
+  APP_ROOT = root
+
+  DataMapper::setup(:default, "sqlite3://#{root}/the_app.db")
+
   get '/' do
     'yo'
   end
+  run! if app_file == $0
 end
